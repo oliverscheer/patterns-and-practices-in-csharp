8ced557b73fe796aa0963109a12fcbd7ff71d5b0
Oliver Scheer <oliver.scheer@hotmail.de>
2024-12-28 09:41:12 +0100

    Add sample Null Conditional Operator

---
+++ OliverScheer.PatternsAndPractices.CodingThings/NullConditionalOperator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OliverScheer.PatternsAndPractices.CodingThings
+{
+    public class NullConditionalOperator
+    {
+        public void Sample()
+        {
+
+            var customer = new Customer();
+
+
+            // Old Version
+            if (customer is not null && customer.Orders is not null)
+            {
+                customer.Orders.Ship();
+            }
+
+            // New Version
+            customer?.Orders?.Ship();
+
+        }
+    }
+
+    public class Customer
+    {
+        public Customer()
+        {
+        }
+
+        public Orders Orders { get; internal set; }
+    }
+
+    public class Orders
+    {
+        public void Ship()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
